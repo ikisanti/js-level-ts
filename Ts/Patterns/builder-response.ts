3b1c817dc9329ac53d5fbd553b8c8a30c1fb6618
Santiago <ikisanti23@gmail.com>
2025-08-14 12:24:41 -0500

    Add ResponseBuilder class for constructing API response objects

---
+++ Ts/Patterns/builder-response.ts
@@ -0,0 +1,54 @@
+
+class ResponseBuilder {
+    private status: "success" | "error" = "success";
+    private data: any = null;
+    private message: string = "";
+
+    static create() {
+        return new ResponseBuilder();
+    }
+
+    ok(data: any) {
+        this.status = "success";
+        this.data = data;
+        return this;
+    }
+
+    error(msg: string) {
+        this.status = "error";
+        this.message = msg;
+        this.data = null;
+        return this;
+    }
+
+    messageText(msg: string) {
+        this.message = msg;
+        return this;
+    }
+
+    build() {
+        return {
+            status: this.status,
+            data: this.data,
+            message: this.message
+        };
+    }
+}
+
+// --- Endpoints backend Simulation  ---
+function getUser() {
+    return ResponseBuilder.create()
+        .ok({ id: 1, name: "Alice" })
+        .messageText("User found")
+        .build();
+}
+
+function getError() {
+    return ResponseBuilder.create()
+        .error("User not found")
+        .build();
+}
+
+// --- Testing ---
+console.log("✅ Usuario encontrado:", getUser());
+console.log("🚫 Usuario no encontrado:", getError());
